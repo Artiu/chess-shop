878eadc1bd2082183a6516f725d21177c5c59778
Artiu <71273256+Artiu@users.noreply.github.com>
2021-05-28 21:14:53 +0200

    whole template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './navbar/Navbar';
-import HomePage from './home-page/HomePage'
+import HomePage from './home-page/HomePage';
+import AboutUs from './about-us/AboutUs';
+import Shop from './shop/shop';
+import Footer from './footer/Footer';
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <>
+        <Navbar />
+        <HomePage />
+        <AboutUs />
+        <Shop />
+        <Footer />
+      </>
+    );
+  }
+};
 
 ReactDOM.render(
-  <>
-    <Navbar />
-    <HomePage />
-  </>,
+  <App/>,
   document.getElementById('root')
 );
---
+++ src/shop/shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './shop.css';
+export default class Shop extends React.Component
+{
+    render(){
+        return(
+            <div className='shop'>
+                
+            </div>
+        );
+    }
+};
